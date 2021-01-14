f36b2fe0acdbdab231958c6b8b2d4a392d35a9dd
Vitor Machado Vieira <vmvieira55@gmail.com>
2021-01-13 23:31:39 -0300

    instalações iniciais necessárias

---
+++ src/components/TestHeader.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const TestHeader = (props) => {
+  return (
+    <header>
+      <img
+        className="img-fluid my-4"
+        src="https://juit.io/assets/images/lp/logo-juit-rimor.png"
+        alt="JUIT logo"
+      />
+      <h2 className="my-2">Teste Juit: {props.testType} </h2>
+    </header>
+  );
+};
+
+export default TestHeader;
